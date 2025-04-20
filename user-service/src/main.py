0cb4db30019a940ace473ead76f3295bba8917d2
mattoranking <mattoranking@gmail.com>
2025-04-21 01:32:54 +0200

    user-service: setup user service
    Signed-off-by: mattoranking <mattoranking@gmail.com>

--- user-service/src/main.py
+++ user-service/src/main.py
@@ -9,7 +9,7 @@ def create_app() -> FastAPI:
         description="Welcome to the User Service API documentation."
                     "Here you will be able to discover all of the "
                     "ways you can interact with the User API.",
-        root_path="/api/v1",
+        root_path="/v1",
     )
 
     app.include_router(router)
